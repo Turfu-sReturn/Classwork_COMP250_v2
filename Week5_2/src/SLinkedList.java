1bb3a9cb7c7af68f10fd7222c4b34a22cd033ece
Joe1651 <jonathan.duplantis@hotmail.com>
2025-09-25 11:01:09 -0400

    Class 25/09

---
+++ Week5_2/src/SLinkedList.java
@@ -0,0 +1,68 @@
+public class SLinkedList {
+    private SNode head;
+    private SNode tail;
+    private int size;
+
+    private class SNode {
+        String element;
+        SNode next;
+    }
+
+    public void addFirst(String e) {
+        SNode newNode = new SNode();
+        newNode.element = e;
+        newNode.next = head;
+        // edge case
+        if (head == null)
+            tail = newNode;
+        head = newNode;
+        size += 1;
+    }
+
+    public void addLast(String e) {
+        // Create a new node
+        SNode newNode = new SNode();
+        newNode.element = e;
+
+        // Add it to the end
+        tail.next = newNode;
+        tail = newNode;
+        size += 1;
+    }
+
+    public String removeLast() {
+        // Edge cases
+        if (size == 0)
+            throw new IllegalArgumentException("The list is empty.");
+        if (size == 1) {
+            SNode temp = tail;
+            tail = null;
+            head = null;
+            size -= 1;
+            return temp.element;
+        }
+
+        // get the reference needed to update the tail
+        SNode temp = tail;
+        SNode newTail = null;
+        for (int i = 0; i < this.size; i++) {
+            if (newTail == null) {
+                newTail = head;
+                continue;
+            }
+            if (newTail.next == temp) {
+                break;
+            }
+
+            newTail = newTail.next;
+        }
+
+        // update the fields
+        tail = newTail;
+        tail.next = null;
+        size -= 1;
+
+        // return the element removed
+        return temp.element;
+    }
+}
